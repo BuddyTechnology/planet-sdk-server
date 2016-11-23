7baf36cce76a79ae9fab3b726b2f53966eb0fe8f
geekfan <bruce.shaoheng@gmail.com>
2016-11-24 00:56:22 +0800

    no message

--- planet-sdk-java/src/test/java/im/facechat/test/TestSendMessage.java
+++ planet-sdk-java/src/test/java/im/facechat/test/TestSendMessage.java
@@ -7,7 +7,7 @@ public class TestSendMessage {
 
 	public static void main(String[] args) {
 		try{
-			System.out.println(PlanetSDK.sendIM("dbc3c0983b58f6ba-8976cca0-c61e-4f66-8ec7-1828b9a85a79","ni hao"));
+			System.out.println(PlanetSDK.sendIM("dbc3c0983b58f6ba-61d16dd5-d030-4d9f-b05f-9b68f60a9bac","ni hao"));
 		}catch(PlanetException e){
 			e.printStackTrace();
 		}
