9ed69dfc2d0971fc3acf08cc10d217980b8c83f3
geekfan <bruce.shaoheng@gmail.com>
2016-11-28 18:51:11 +0800

    no message

--- planet-sdk-java/src/test/java/im/facechat/test/TestSendApns.java
+++ planet-sdk-java/src/test/java/im/facechat/test/TestSendApns.java
@@ -1,13 +1,25 @@
 package im.facechat.test;
 
+import im.facechat.planet.cons.PlanetConfig;
 import im.facechat.planet.exp.PlanetException;
+import im.facechat.planet.msg.Apns;
 import im.facechat.planet.sdk.PlanetSDK;
 
 public class TestSendApns {
 
 	public static void main(String[] args) {
 		try{
-			System.out.println(PlanetSDK.sendIM("dbc3c0983b58f6ba-f367b48c-845f-4602-b275-afbac81ce6a0","ni hao",0));
+			PlanetConfig.appId = "582e75b7b80baa67261354e9";
+			PlanetConfig.appToken = "3d922c108c521a6605969653528ca7ca";
+			
+			
+			Apns apns = new Apns();
+			apns.setAwake(true);
+			apns.setBackgroud(true);
+			apns.setContent("Hello");
+			apns.setTitle("Tiki-马甲");
+			apns.setIncrBadges(1);
+			System.out.println(PlanetSDK.sendApns("74281bf4333d-1a2857c6-9fc9-4641-9e05-5ab5d0a76af3",apns));
 		}catch(PlanetException e){
 			e.printStackTrace();
 		}
