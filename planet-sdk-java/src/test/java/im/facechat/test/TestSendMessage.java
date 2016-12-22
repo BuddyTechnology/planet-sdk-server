69cc16587fbf0cb69a298a92d3508ac83dfa0812
geekfan <bruce.shaoheng@gmail.com>
2016-12-22 15:54:26 +0800

    no message

--- planet-sdk-java/src/test/java/im/facechat/test/TestSendMessage.java
+++ planet-sdk-java/src/test/java/im/facechat/test/TestSendMessage.java
@@ -18,7 +18,8 @@ public class TestSendMessage {
 			apns.setContent("Hello");
 			apns.setTitle("Tiki-马甲");
 			
-			System.out.println(PlanetSDK.sendApns("66fd89018de2-5244b88c-317d-48fe-b2e7-0927c969f808", apns));
+//			System.out.println(PlanetSDK.sendApns("66fd89018de2-5244b88c-317d-48fe-b2e7-0927c969f808", apns));
+			PlanetSDK.sendIM("66fd89018de2-40b75682-9f2f-4780-9ce8-66c163d494aa","hello",0);
 		}catch(PlanetException e){
 			e.printStackTrace();
 		}
