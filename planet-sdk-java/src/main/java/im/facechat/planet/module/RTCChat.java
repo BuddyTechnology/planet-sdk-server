29d3836f08760ac734b6e2b9b8c1044d77db3c0e
geekfan <bruce.shaoheng@gmail.com>
2017-01-06 17:32:26 +0800

    no message

--- planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
+++ planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
@@ -11,9 +11,22 @@ public class RTCChat {
 	
 	public long end;//最后一次聊天的时间
 	
-	public boolean finished;//是否已经结束
+	public boolean hadCalled;//是否已经结束
 	
 	public List<String> members;//通话的两个人
+	
+	public String nt;//netType类型，2G,3G,4G,wifi
+	public String isp;//运营商,netProvider
+	public double[] locs;//lng,lat
+	public String ip;
+	public long rsb;//remindSendBandwidth,剩余上行带宽
+	public long rrb;//remindReceiveBandWidth,剩余下行带宽
+	public long sr;//sendRate,发送速度
+	public long rr;//receiveRate,接收速度
+	public float sl;//sendLoss,发送丢包率
+	public float rl;//receiveLoss,接收丢包率
+	public int link;//当前用户使用的连接类型host,p2p,relay
+	public int quality;//高清，标清，普通
 
 	public long getStart() {
 		return start;
@@ -30,13 +43,21 @@ public class RTCChat {
 	public void setEnd(long end) {
 		this.end = end;
 	}
-
-	public boolean isFinished() {
-		return finished;
+	
+	public String getSession() {
+		return session;
 	}
 
-	public void setFinished(boolean finished) {
-		this.finished = finished;
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public boolean isHadCalled() {
+		return hadCalled;
+	}
+
+	public void setHadCalled(boolean hadCalled) {
+		this.hadCalled = hadCalled;
 	}
 
 	public List<String> getMembers() {
