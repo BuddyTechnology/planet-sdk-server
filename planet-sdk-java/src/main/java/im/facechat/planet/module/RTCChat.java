2c4a1a46853abff650d83e58ba10e55a0b136409
geekfan <bruce.shaoheng@gmail.com>
2017-01-06 17:33:11 +0800

    no message

--- planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
+++ planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 
 public class RTCChat {
-	
 	public String session;
-	
 	public long start;//开始聊天的时间
-	
 	public long end;//最后一次聊天的时间
-	
 	public boolean hadCalled;//是否已经结束
-	
 	public List<String> members;//通话的两个人
-	
 	public String nt;//netType类型，2G,3G,4G,wifi
 	public String isp;//运营商,netProvider
 	public double[] locs;//lng,lat
@@ -27,6 +21,102 @@ public class RTCChat {
 	public float rl;//receiveLoss,接收丢包率
 	public int link;//当前用户使用的连接类型host,p2p,relay
 	public int quality;//高清，标清，普通
+
+	public String getNt() {
+		return nt;
+	}
+
+	public void setNt(String nt) {
+		this.nt = nt;
+	}
+
+	public String getIsp() {
+		return isp;
+	}
+
+	public void setIsp(String isp) {
+		this.isp = isp;
+	}
+
+	public double[] getLocs() {
+		return locs;
+	}
+
+	public void setLocs(double[] locs) {
+		this.locs = locs;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public long getRsb() {
+		return rsb;
+	}
+
+	public void setRsb(long rsb) {
+		this.rsb = rsb;
+	}
+
+	public long getRrb() {
+		return rrb;
+	}
+
+	public void setRrb(long rrb) {
+		this.rrb = rrb;
+	}
+
+	public long getSr() {
+		return sr;
+	}
+
+	public void setSr(long sr) {
+		this.sr = sr;
+	}
+
+	public long getRr() {
+		return rr;
+	}
+
+	public void setRr(long rr) {
+		this.rr = rr;
+	}
+
+	public float getSl() {
+		return sl;
+	}
+
+	public void setSl(float sl) {
+		this.sl = sl;
+	}
+
+	public float getRl() {
+		return rl;
+	}
+
+	public void setRl(float rl) {
+		this.rl = rl;
+	}
+
+	public int getLink() {
+		return link;
+	}
+
+	public void setLink(int link) {
+		this.link = link;
+	}
+
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
 
 	public long getStart() {
 		return start;
