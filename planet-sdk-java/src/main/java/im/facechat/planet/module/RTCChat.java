de4d027d0a56a687d35a51777ab7a55a02f8fd1d
geekfan <bruce.shaoheng@gmail.com>
2017-01-17 23:02:09 +0800

    no message

--- planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
+++ planet-sdk-java/src/main/java/im/facechat/planet/module/RTCChat.java
@@ -1,6 +1,5 @@
 package im.facechat.planet.module;
 
-import java.util.List;
 
 
 public class RTCChat {
@@ -10,11 +9,8 @@ public class RTCChat {
 	public long end;//最后一次聊天的时间
 	public boolean finished;//是否已经结束
 	public boolean suctalk;
-	public List<String> members;//通话的两个人
 	public String nt;//netType类型，2G,3G,4G,wifi
 	public String isp;//运营商,netProvider
-	public String locs;//lng-lat
-	public String ip;
 	public long rsb;//remindSendBandwidth,剩余上行带宽
 	public long rrb;//remindReceiveBandWidth,剩余下行带宽
 	public long sr;//sendRate,发送速度
@@ -23,14 +19,6 @@ public class RTCChat {
 	public float rl;//receiveLoss,接收丢包率
 	public int link;//当前用户使用的连接类型host,p2p,relay
 	public int quality;//高清，标清，普通
-
-	public String getLocs() {
-		return locs;
-	}
-
-	public void setLocs(String locs) {
-		this.locs = locs;
-	}
 
 	public String getNt() {
 		return nt;
@@ -47,15 +35,7 @@ public class RTCChat {
 	public void setIsp(String isp) {
 		this.isp = isp;
 	}
-
-	public String getIp() {
-		return ip;
-	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-
+	
 	public long getRsb() {
 		return rsb;
 	}
@@ -151,14 +131,4 @@ public class RTCChat {
 	public void setFinished(boolean finished) {
 		this.finished = finished;
 	}
-
-	public List<String> getMembers() {
-		return members;
-	}
-
-	public void setMembers(List<String> members) {
-		this.members = members;
-	}
-	
-	
 }
